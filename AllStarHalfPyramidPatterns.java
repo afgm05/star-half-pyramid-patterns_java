dcbb9728c0b376bda5221c945755187f15b2fa0b
Faye Manalo <aly.faye05@gmail.com>
2022-08-17 19:00:53 +0800

    2nd commit

--- AllStarHalfPyramidPatterns.java
+++ AllStarHalfPyramidPatterns.java
@@ -8,19 +8,17 @@ public class AllStarHalfPyramidPatterns {
         System.out.print("Enter an integer --> ");
         int number = sc.nextInt();		
 		
-		//Star-half-pyramid
-		        
-        for (int i = 1; i <= number; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print('*');
-            }
-            System.out.println();         
-        }  
+		//Star-half-pyramid		        
+    	for (int i = 1; i <= number; i++) {
+        	for (int j = 1; j <= i; j++) {
+            	System.out.print('*');
+        	}
+        	System.out.println();         
+    	}  
 
 		System.out.println();
 
-		//Star-half-pyramid-inverted
-		
+		//Star-half-pyramid-inverted		
 		for (int i = number; i > 0; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print('*');
@@ -30,8 +28,7 @@ public class AllStarHalfPyramidPatterns {
 		
 		System.out.println();
 		
-		//Star-half-pyramid-mirrored
-		
+		//Star-half-pyramid-mirrored		
 		for (int i = 1; i <= number; i++) {
 			
             for (int j = number-i; j > 0; j--) {
@@ -41,16 +38,13 @@ public class AllStarHalfPyramidPatterns {
             for (int k = i; k > 0; k--) {
                 System.out.print('*');
             }      
-             System.out.println();    
-			 
+             System.out.println();    			 
         }   
 
 		System.out.println();
 		
-		//Star-half-pyramid-inverted-mirrored
-		
-		for (int i = number; i > 0; i--) {
-            
+		//Star-half-pyramid-inverted-mirrored		
+		for (int i = number; i > 0; i--) {            
             for (int j = 0; j < number-i; j++) {           
                 System.out.print(' '); 
             }
